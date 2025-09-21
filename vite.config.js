04419ef7ae16e5ee4dcf14eb1f338349ccadf138
r4cs <r.guzansky@hotmail.com>
2025-09-21 18:38:34 -0300

    adaptacoes p prod no vercel

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+// !!! PROD !!!
 // vite.config.js
 import { defineConfig } from 'vite';
 import path from 'path';
@@ -9,22 +10,51 @@ export default defineConfig({
     outDir: 'dist',
     assetsDir: 'assets',
     sourcemap: false,
-    rollupOptions: {
-      input: {
-        main: path.resolve(__dirname, 'public/index.html') // ✅ ESPECIFICA O INDEX.HTML
-      }
-    }
+    emptyOutDir: true // ✅ Limpa a pasta dist antes de buildar
   },
   server: {
     port: 3000,
-    open: '/public/index.html',
-    // open: true,
-    host: true // Permite acesso externo
+    open: true,
+    host: true
   },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
       '@assets': path.resolve(__dirname, './assets')
     }
-  }
+  },
+  // ✅ Configuração importante para a Vercel
+  base: './' // Usa caminhos relativos
 });
+
+// !!! LOCAL !!!
+// vite.config.js
+// import { defineConfig } from 'vite';
+// import path from 'path';
+
+// export default defineConfig({
+//   root: '.',
+//   publicDir: 'public',
+//   build: {
+//     outDir: 'dist',
+//     assetsDir: 'assets',
+//     sourcemap: false,
+//     rollupOptions: {
+//       input: {
+//         main: path.resolve(__dirname, 'public/index.html') // ✅ ESPECIFICA O INDEX.HTML
+//       }
+//     }
+//   },
+//   server: {
+//     port: 3000,
+//     open: '/public/index.html',
+//     // open: true,
+//     host: true // Permite acesso externo
+//   },
+//   resolve: {
+//     alias: {
+//       '@': path.resolve(__dirname, './src'),
+//       '@assets': path.resolve(__dirname, './assets')
+//     }
+//   }
+// });
